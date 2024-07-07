c44d6aa74fd2d403d03bb607c81bd39a9ff5bffa
zalamon <lehoailinh2206@gmail.com>
2024-07-08 05:07:09 +0700

    update

--- src/modules/auth/google.strategy.ts
+++ src/modules/auth/google.strategy.ts
@@ -11,7 +11,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
         super({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: 'http://localhost:8081/auth/redirect',
+            callbackURL: 'https://api.oziny.com',
             scope: ['email', 'profile'],
         });
     }
